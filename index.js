28c811811a1e0cfd86cdcefd0639e50a2fbe8de7
Richard Zhang <1501109542@qq.com>
2023-05-16 12:11:08 +0800

    chore: finish

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const playGround = document.querySelector('.playground')
+const playground = document.querySelector('.playground')
 const list = document.querySelector('.list')
 const items = document.querySelectorAll('.list-item')
 
@@ -6,6 +6,34 @@ const animationMap = new Map()
 
 function updateMap() {
   animationMap.clear()
+  const playgroundRect = playground.getBoundingClientRect()
+  const scrollStart = playgroundRect.top + window.scrollY
+  const scrollEnd = playgroundRect.bottom + window.scrollY - window.innerHeight
+  for (const item of items) {
+    animationMap.set(item, getDomAnimation(scrollStart, scrollEnd, item))
+  }
+}
+
+function getDomAnimation(scrollStart, scrollEnd, dom) {
+  scrollStart = dom.dataset.order * 400 + scrollStart
+
+  const opacityAnimation = createAnimation(scrollStart, scrollEnd, 0, 1)
+  const opacity = function (scroll) {
+    return opacityAnimation(scroll)
+  }
+
+  const xAnimation = createAnimation(scrollStart, scrollEnd, list.clientWidth / 2 - dom.offsetLeft - dom.clientWidth / 2, 0)
+  const yAnimation = createAnimation(scrollStart, scrollEnd, list.clientHeight / 2 - dom.offsetTop - dom.clientHeight / 2, 0)
+
+  const scaleAnimation = createAnimation(scrollStart, scrollEnd, 0.5, 1)
+  const transform = function (scroll) {
+    return `translate(${xAnimation(scroll)}px, ${yAnimation(scroll)}px) scale(${scaleAnimation(scroll)})`
+  }
+
+  return {
+    opacity,
+    transform,
+  }
 }
 
 function createAnimation(scrollStart, scrollEnd, valStart, valEnd) {
@@ -31,7 +59,7 @@ function updateStyles() {
     }
   }
 }
-
+updateMap()
 updateStyles()
 
 window.addEventListener('scroll', updateStyles)
